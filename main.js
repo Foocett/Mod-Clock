a1255f4606b760c06be35d63e08a1adbca868838
foocett <ajfawcett@gmail.com>
2024-06-12 21:43:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+const port = 3000;
 const express = require('express');
 const app = express();
-const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/Public/index.html');
 });
-
 app.use(express.static('Public'));
 
-app.listen(port, () => {
-    console.log(`Web server running at http://localhost:${port}`);
+io.on("connection", (socket) => {
+    socket.on("test", () => {
+        console.log("test")
+    })
 });
+
+server.listen(port, () => {
+    console.log(`Web server running at http://localhost:${port}`);
+})
