fd966f4e0a73d054e1c3e26bc530bc5bb463892c
foocett <ajfawcett@gmail.com>
2024-07-05 09:53:26 -0500

    Update documentation

--- startClock.sh
+++ startClock.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
-#line one is called a shebang or hashbang
-#it's super common in shell scripts and allows for easier use in a lot of ways
-#see readme file for more information
+# this first line is called a shebang or hashbang, and it must occur on the first line of the shell script
+# see readme file for more information
 
-#for debugging
+# This file is called a shell script, read about unix shell and shell scripts in the readme file
+
+
+# for debugging
 echo 0
 
-#navigate to git repository on raspberry pi
+# navigate to local git repository on raspberry pi
+# shellcheck disable=SC2164 #(this is to stop my code editor from yelling at me)
 cd /home/clocc/Mod-Clock
 
-#save local changes (this is to maintain content in json files)
-#pull latest version from git
-#apply the saved data from earlier
+# git is an open source version control system (VCS) and one of the most used programming tools out there
+# see readme for more information about git
+
+# save local changes (this is to maintain content in json files)
 git stash
+# pull latest version from git
 git pull
+# apply the saved data from earlier (this is to ensure that data inside of files like userdata.json is not lost)
 git stash apply
 
-#update any libraries in package.json
+
+# node package manager (npm) is another super helpful tool for managing and installing node packages, read more in readme.md
+# update any libraries listed in package.json
 npm install
 
 
-#wait two seconds before going on to the next step
+# wait two seconds before going on to the next step
 sleep 2
 
-#tells node that this is a production environment
+# tells node that this is a production environment
 export NODE_ENV=production
 
-#this line executes the main.js file and starts the server
-#the '&' at the end executes the task as background process, without this, the next line wouldn't execute until the server closes and the script terminates
+# this line executes the main.js file and starts the server
+# the '&' at the end executes the task as background process, without this, the next line wouldn't execute until the server closes and the script terminates
 /home/clocc/.nvm/versions/node/v20.15.0/bin/node main.js &
 
 
-#this line opens up the webpage in an instance of chromium browser, starting in fullscreen in kiosk mode (kiosk basically locks the page)
+# this line opens up the webpage in an instance of chromium browser, starting in fullscreen in kiosk mode (kiosk basically locks the page)
 /usr/bin/chromium-browser --kiosk http://localhost:3000/ --start-fullscreen
